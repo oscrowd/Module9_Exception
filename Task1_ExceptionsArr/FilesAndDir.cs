578c6197303cb0c339607dae3cd2fccae4c9ec3b
oscrowd <oscrowd@gmail.com>
2023-04-14 12:10:29 +0300

    Добавьте файлы проекта.

---
+++ Task1_ExceptionsArr/FilesAndDir.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_ExceptionsArr
+{
+    public class FilesAndDir
+    {
+        public string DirPath;
+        //{
+        //    get
+        //    {
+        //        return DirPath;
+        //    }
+        //    set
+        //    {
+        //        DirPath = value;
+        //    }
+        //}
+
+        public Boolean DirExist()
+        {
+            var di = new DirectoryInfo(DirPath);
+            return di.Exists;
+        }
+        public void DelPathAndDir()
+        {
+            //path = DirPath;
+            var di = new DirectoryInfo(DirPath);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+        }
+    }
+
+}
---
+++ Task1_ExceptionsArr/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_ExceptionsArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Boolean DirEx = false;
+                string str="";
+                ExceptionArr exceptionArr = new ExceptionArr(str);
+                //HumanException humanException = new HumanException();
+                FilesAndDir filesAndDir = new FilesAndDir();
+                Console.WriteLine("Введите путь к директори:");
+                filesAndDir.DirPath = Console.ReadLine();
+                DirEx = filesAndDir.DirExist();
+
+                if (DirEx)
+                {
+                    filesAndDir.DelPathAndDir();
+                    Console.WriteLine("Удалены папки и файлы в директории {0}", filesAndDir.DirPath);
+
+                }
+                else
+                {
+                    exceptionArr.GetBaseException("sdfasdfsd");
+                }
+
+
+            }
+            catch (FormatException) 
+            {
+            }
+            catch (ArgumentException)
+            { 
+            }
+            catch (DriveNotFoundException)
+            {
+            }
+            catch (DivideByZeroException)
+            {
+            }
+            catch (FileNotFoundException)
+            {
+            }
+
+            finally
+            { 
+            
+             }
+        }
+    }
+}
+
+
+
