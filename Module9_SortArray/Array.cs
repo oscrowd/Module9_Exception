63e66c62c311947c75647cde0cd8abe7ecf9123c
oscrowd <oscrowd@gmail.com>
2023-04-14 23:59:42 +0300

    Module9_SortArray_2

---
+++ Module9_SortArray/Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module9_SortArray
+{
+    public class Array
+    {
+        public string[] GetArrayFromConsole()
+        {
+            var result = new string[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = Console.ReadLine();
+            }
+
+            return result;
+        }
+    }
+}
--- Module9_SortArray/Program.cs
+++ Module9_SortArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,46 @@ using System.Threading.Tasks;
 
 namespace Module9_SortArray
 {
+    class Person
+    {
+        public string Name { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            string[] FIO;
+            Console.WriteLine("Введите 5 ФИО для сортировки:");
+            //Записываем в массив
+            Array array = new Array();
+            FIO = array.GetArrayFromConsole();
+
+            Console.WriteLine("Начальный массив");
+
+            foreach (string fio in FIO)
+            {
+
+                // Display Original List
+                Console.WriteLine(fio);
+            }
+            Console.ReadLine();
+
+            IEnumerable<string> FIO1 = from fio in FIO
+                                        orderby fio.Substring(0, 1) descending
+                                        select fio;
+            Console.WriteLine("Отсортированный список массив");
+
+            foreach (string fio1 in FIO1)
+            {
+
+                // Display Original List
+                Console.WriteLine(fio1);
+            }
+            Console.ReadLine();
+
         }
     }
+
+   
 }
