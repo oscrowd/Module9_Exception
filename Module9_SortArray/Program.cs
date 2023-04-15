fe880116e66218b7e76185f02a0fd9df838c2f8a
oscrowd <oscrowd@gmail.com>
2023-04-15 12:24:07 +0300

    Module9_SortArray_NotCompleted

--- Module9_SortArray/Program.cs
+++ Module9_SortArray/Program.cs
@@ -18,34 +18,76 @@ namespace Module9_SortArray
 
             string[] FIO;
             Console.WriteLine("Введите 5 ФИО для сортировки:");
-            //Записываем в массив
-            //dfsdfsd
+            //Записываем в массив ФИО
             Array array = new Array();
             FIO = array.GetArrayFromConsole();
-
             Console.WriteLine("Начальный массив");
-
+            
             foreach (string fio in FIO)
             {
 
                 // Display Original List
                 Console.WriteLine(fio);
             }
-            Console.ReadLine();
-
-            IEnumerable<string> FIO1 = from fio in FIO
+            //Получаем число и сортируем
+            NumberSort numberSort = new NumberSort();
+            numberSort.NumberSortEvent += SortNum;
+            while (true)
+            {
+                try
+                {
+                    numberSort.ReadSort();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неправильно! Введите 1 или 2: ");
+                }
+            }
+            static void SortNum(int number)
+            { 
+                switch (number) 
+                {
+                    case 1: IEnumerable<string> FIO1 = from fio in FIO
                                         orderby fio.Substring(0, 1) descending
                                         select fio;
-            Console.WriteLine("Отсортированный список массив");
+                    break;
+                    case 2: IEnumerable<string> FIO1 = from fio in FIO
+                                        orderby fio.Substring(0, 1) ascending
+                                        select fio;
+                    break;
+                }
+            }
+            
 
-            foreach (string fio1 in FIO1)
-            {
+            //IEnumerable<string> FIO1 = from fio in FIO
+            //                            orderby fio.Substring(0, 1) descending
+            //                            select fio;
+            //Console.WriteLine("Отсортированный список массив");
+
+            //foreach (string fio1 in FIO1)
+            //{
 
                 // Display Original List
-                Console.WriteLine(fio1);
-            }
-            Console.ReadLine();
+            //    Console.WriteLine(fio1);
+            //}
+        }
+        class NumberSort
+        {
+            public delegate void NumberSortDelegate();
+            public event NumberSortDelegate NumberSortEvent;
 
+            void ReadSort()
+            { 
+                Console.WriteLine();
+                Console.WriteLine("Введите число 1 или число2: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > 2 || number < 1) throw new FormatException();
+                NumberSort(number);
+            }
+            protected virtual void NumberSort(int number) 
+            {
+                NumberSortEvent?.Invoke(number);
+            }
         }
     }
 
